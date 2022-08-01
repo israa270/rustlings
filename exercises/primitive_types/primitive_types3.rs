d6f1e3b550ae7f5486bc3409fa23d50f9d3a86ab
israa270 <isra.elghazawy@gmail.com>
2022-08-01 06:37:18 +0200

    feat: solve basic rust exe

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,14 +2,22 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let a = ???
-
+    let a = create_array();
+    
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
     }
+}
+
+fn create_array() -> Vec<i32>{
+  let mut a = Vec::new();
+    for  i in 0..100{
+        a.push(i);
+    }
+    a
 }
